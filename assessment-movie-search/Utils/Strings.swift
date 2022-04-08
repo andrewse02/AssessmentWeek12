a4dc2931f8c7af1daf58e11f39b37c7b2fc77b41
Andrew Elliott <andrewelliott159@gmail.com>
2022-04-08 17:47:43 -0600

    Fix Strings

---
+++ assessment-movie-search/Utils/Strings.swift
@@ -0,0 +1,20 @@
+//
+//  Strings.swift
+//  assessment-movie-search
+//
+//  Created by Andrew Elliott on 4/8/22.
+//
+
+import Foundation
+
+class Strings {
+    static let baseURL = "https://api.themoviedb.org"
+    static let imageBaseURL = "https://image.tmdb.org/t/p/original"
+    static let apiVersion = "/3"
+    static let searchEnpoint = "/search"
+    static let movieEndpoint = "/movie"
+    static let apiKeyQuery = "api_key"
+    static let apiKey = "9bf996aca10683814be88f5ce3827a0a"
+    static let queryQuery = "query" // lol
+    
+}
